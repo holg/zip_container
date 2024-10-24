92d8912b0f4cc00454a15b32745064cef512adbc
Holger Trahe <trahe@mac.com>
2024-10-24 16:05:54 +0200

    [ADD] added a prerelease section, not needing linux , added     if env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "arm" {         println!("cargo:rustc-cfg=__ARM_ARCH");     } to debug the issue for linux not building ring, due to ARM assembly issues

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
+use std::env;
+// use std::process::Command;
 #[cfg(feature = "python")]
 fn main() {
     // Add the extension module link arguments only when the 'python' feature is enabled.
     use pyo3_build_config;
     pyo3_build_config::add_extension_module_link_args();
+    if env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "arm" {
+        println!("cargo:rustc-cfg=__ARM_ARCH");
+    }
 }
 
 #[cfg(not(feature = "python"))]
